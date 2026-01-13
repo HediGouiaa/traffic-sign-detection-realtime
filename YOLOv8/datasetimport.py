7954d86694b28fb66e880edaa42b3c8203df5108
HediGouiaa <hedi.gouiaa@enis.tn>
2026-01-13 16:54:47 +0100

    del

--- YOLOv8/datasetimport.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="Dbbmn5uZ3G2QVQx8zIKi")
-project = rf.workspace("roboflow-100").project("road-signs-6ih4y")
-version = project.version(2)
-dataset = version.download("yolov5")
